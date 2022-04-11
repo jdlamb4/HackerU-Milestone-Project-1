723c27b2416ae9486a6166b7d6e2e85aaf4d4099
Jacob Lamb <jacoblamb15@gmail.com>
2022-04-11 18:55:05 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,3 @@
+// the website needs to have some questions preloaded. So I need to figure out a way to pull questions from a source, upload them to the website or hardcode in the mean time
+
+// If I want to set it up like Wordle, I'll need to have
