76b1a185084b25fecfa96d7c9f7d93d7591cc5d6
Jacob Lamb <jacoblamb15@gmail.com>
2022-04-18 21:33:30 -0400

    working mascot, clean code later

---
+++ home.js
@@ -0,0 +1,103 @@
+function newImage(url){
+    let image = document.createElement('img')
+    image.src = url
+    image.style.position = 'absolute'
+    document.body.append(image)
+    return image
+}
+
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        function moveCharacter(){ 
+            if(direction === 'west'){
+                x-=1
+            }
+            if(direction === 'north'){
+                y+=1
+            }
+            if(direction === 'east'){
+                x+=1
+            }
+            if(direction === 'south'){
+                y-=1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            callback(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
+    }
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }
+}
+
+function newPlayableCharacter(x, y) {
+    const element = newImage('assets/green-character/static.gif')
+    element.style.zIndex = 1;
+
+    function handleDirectionChange(direction) {
+        if (direction === null) {
+            element.src = `assets/green-character/static.gif`
+        }
+        if (direction === 'west') {
+            element.src = `assets/green-character/west.gif`
+        }
+        if (direction === 'north') {
+            element.src = `assets/green-character/north.gif`
+        }
+        if (direction === 'east') {
+            element.src = `assets/green-character/east.gif`
+        }
+        if (direction === 'south') {
+            element.src = `assets/green-character/south.gif`
+        }
+    }
+
+    move(element).withArrowKeys(x, y, handleDirectionChange)
+
+    return {
+        element: element
+    }
+}
+
+const mascot = newPlayableCharacter(100,110)
